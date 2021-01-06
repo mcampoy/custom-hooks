ef55f82e8fac1968767d35aae16863d6510a3b32
Matías Campoy <matiascampoy@gmail.com>
2021-01-06 15:25:42 -0300

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,27 @@
+import {
+    useState
+} from "react"
+
+
+export const useForm = (initialState = {}) => {
+
+    const reset = () => {
+        setValues(initialState)
+    }
+
+    const [values, setValues] = useState(initialState)
+
+    const handleInputChange = ({
+        target
+    }) => {
+
+        setValues({
+            ...values,
+            [target.name]: target.value
+        })
+
+    }
+
+    return [values, handleInputChange, reset]
+
+}
